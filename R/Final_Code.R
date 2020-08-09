15aa6134f32a9f5fb033ffa845592fb7fec65012
Clay Gendron <claygendron@Clays-MacBook-Pro.local>
2020-08-09 09:40:13 -0400

    updates

--- R/Final_Code.R
+++ R/Final_Code.R
@@ -18,5 +18,11 @@ gift_hist <- hist(pva$GIFTAMNT,
                   breaks = c(0,10,20,30,40,50,100,200))
 
 cor_pva <- cor(pva)
-cor_pva
+gift_corrplot <- corrplot(cor_pva, method = 'circle')
 
+n_gift_hist <- hist(pva$NGIFTALL,
+                  main = 'Frequency of Total Number of Gifts',
+                  xlab = 'Total Number of Gifts',
+                  ylab = 'Frequency',
+                  col = 'orangered',
+                  freq = TRUE)
